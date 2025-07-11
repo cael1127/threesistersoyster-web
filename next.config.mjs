76653aa4beba255dfa1d3df29d73efa9d8fb34e2
Cael Findley <caelfindley@gmail.com>
2025-07-11 08:14:31 -0500

    Maybe fixes

--- next.config.mjs
+++ next.config.mjs
@@ -27,6 +27,12 @@ const nextConfig = {
       },
     ]
   },
+  webpack: (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = config.resolve.alias || {};
+    config.resolve.alias['@'] = __dirname;
+    return config;
+  },
 }
 
 export default nextConfig
