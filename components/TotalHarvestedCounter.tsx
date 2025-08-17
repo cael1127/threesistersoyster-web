ba6ecd40b8c364e19ce2a0a4e22e84d4d03c8904
Cael Findley <caelfindley@gmail.com>
2025-08-17 13:49:26 -0500

    Creating more ux designs

--- components/TotalHarvestedCounter.tsx
+++ components/TotalHarvestedCounter.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 
 export default function TotalHarvestedCounter() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);//Change this to change the total harvested
   const [displayCount, setDisplayCount] = useState(0);
   const [hasInitialized, setHasInitialized] = useState(false);
 
